f52c6325befb2d0e3a2367535c1759ee294530cf
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-11-03 16:03:35 +0500

    exerciseOne

--- TESTS/src/test/java/Base/BaseClass.java
+++ TESTS/src/test/java/Base/BaseClass.java
@@ -1,7 +1,11 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseClass {
     private WebDriver driver;
@@ -13,7 +17,16 @@ public class BaseClass {
         driver.get(URL);
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
+        exerciseOne();
         driver.quit();
+    }
+
+
+    public void exerciseOne(){
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List <WebElement> menu = driver.findElements(By.tagName("li"));
+        System.out.println(menu.size());
     }
 
     public static void main(String[] args) {

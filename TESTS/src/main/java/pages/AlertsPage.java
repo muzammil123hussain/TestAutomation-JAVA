c9e4fb14f03313684a84c0c6cb40d6568781ba25
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-11-05 21:32:44 +0500

    Confirm Js alert

--- TESTS/src/main/java/pages/AlertsPage.java
+++ TESTS/src/main/java/pages/AlertsPage.java
@@ -5,19 +5,33 @@ import org.openqa.selenium.WebDriver;
 
 public class AlertsPage {
     private WebDriver driver;
-    private By triggerAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
+    private By triggerOkAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
+    private By triggerConfirmAlertButton = By.xpath(".//button[text()='Click for JS Confirm']");
+    private By triggerPromptAlertButton = By.xpath(".//button[text()='Click for JS Prompt']");
     private By result = By.id("result");
     public AlertsPage(WebDriver driver) {
         this.driver = driver;
     }
-    public void clickTriggerAlertButton(){
-        driver.findElement(triggerAlertButton).click();
+    public void clickTriggerOkAlertButton(){
+        driver.findElement(triggerOkAlertButton).click();
+    }
+    public void clickTriggerConfirmAlertButton(){
+        driver.findElement(triggerConfirmAlertButton).click();
+    }
+    public void clickTriggerPromptAlertButton(){
+        driver.findElement(triggerPromptAlertButton).click();
     }
     public String getAlertTxt(){
         return driver.switchTo().alert().getText();
     }
     public void clickAlertOk(){
         driver.switchTo().alert().accept();
+    }
+    public void clickAlertConfirm(){
+        driver.switchTo().alert().accept();
+    }
+    public void clickAlertCancel(){
+        driver.switchTo().alert().dismiss();
     }
     public String getResultTxt(){
         return driver.findElement(result).getText();
--- TESTS/src/test/java/Alerts/AlertsTests.java
+++ TESTS/src/test/java/Alerts/AlertsTests.java
@@ -9,9 +9,17 @@ public class AlertsTests extends BaseClass {
     @Test
     public void testJsOkAlert(){
         var okAlert = homePage.clickAlertsLink();
-        okAlert.clickTriggerAlertButton();
+        okAlert.clickTriggerOkAlertButton();
         assertEquals(okAlert.getAlertTxt(),"I am a JS Alert","alert text is not as expected");
         okAlert.clickAlertOk();
         assertEquals(okAlert.getResultTxt(),"You successfully clicked an alert","result text is not as expected");
+    }
+    @Test
+    public void testJsOkConfirmAlert(){
+        var okAlert = homePage.clickAlertsLink();
+        okAlert.clickTriggerConfirmAlertButton();
+        assertEquals(okAlert.getAlertTxt(),"I am a JS Confirm","alert text is not as expected");
+        okAlert.clickAlertOk();
+        assertEquals(okAlert.getResultTxt(),"You clicked: Ok","result text is not as expected");
     }
 }
